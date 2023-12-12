e9e5370abbdcf1a8f3c8fb4e66c897ea80277392
sxram <>
2023-12-12 18:24:25 +0100

    increase test polling duration

--- app/src/test/java/org/sxram/kafka/tutorial/streams/StreamsAppIT.java
+++ app/src/test/java/org/sxram/kafka/tutorial/streams/StreamsAppIT.java
@@ -27,7 +27,7 @@ class StreamsAppIT {
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("group.id", "kafka-java-getting-started");
-        new MyConsumer(outputTopic, props, handlerMock, Duration.ofSeconds(3)).consume();
+        new MyConsumer(outputTopic, props, handlerMock, Duration.ofSeconds(6)).consume();
 
         verify(handlerMock, atLeastOnce()).accept(any());
     }
