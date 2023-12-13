1e76a5a7a7fa39122ff956f223f531d0cb283631
sxram <>
2023-12-13 23:12:11 +0100

    refactor test

--- app/src/test/java/org/sxram/kafka/tutorial/basic/MyProducerConsumerIT.java
+++ app/src/test/java/org/sxram/kafka/tutorial/basic/MyProducerConsumerIT.java
@@ -7,6 +7,7 @@ import org.sxram.kafka.tutorial.Utils;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 
@@ -17,19 +18,21 @@ import static org.sxram.kafka.tutorial.TestUtils.CONFIG_PATH_PREFIX;
 class MyProducerConsumerIT {
 
     @Test
-    void consumesProducedMessageWithHandlerMock() throws IOException {
+    void consumesProducedMessage() throws IOException {
         RecordProcessor<String, String> handlerMock = spy(new RecordProcessor<>());
 
-        new MyProducer(App.TOPIC,
-                Utils.mergeProperties(CONFIG_PATH_PREFIX + App.CLIENT_CONFLUENT_PROPERTIES,
-                        CONFIG_PATH_PREFIX + App.PRODUCER_PROPERTIES)).
-                produce(Files.readAllLines(Paths.get(CONFIG_PATH_PREFIX + App.PRODUCER_INPUT)));
-        new MyConsumer(App.TOPIC,
-                Utils.mergeProperties(CONFIG_PATH_PREFIX + App.CLIENT_CONFLUENT_PROPERTIES,
-                        CONFIG_PATH_PREFIX + App.CONSUMER_PROPERTIES), handlerMock, Duration.ofSeconds(5)).consume();
+        Path producerConfigPath = Paths.get(CONFIG_PATH_PREFIX + App.PRODUCER_INPUT);
 
-        val producerInputFile = CONFIG_PATH_PREFIX + App.PRODUCER_INPUT;
-        verify(handlerMock, times((int) Files.lines(Paths.get(producerInputFile)).count())).accept(any());
+        new MyProducer(App.TOPIC, Utils.mergeProperties(
+                CONFIG_PATH_PREFIX + App.CLIENT_CONFLUENT_PROPERTIES,
+                CONFIG_PATH_PREFIX + App.PRODUCER_PROPERTIES)).produce(Files.readAllLines(producerConfigPath));
+        new MyConsumer(App.TOPIC, Utils.mergeProperties(
+                CONFIG_PATH_PREFIX + App.CLIENT_CONFLUENT_PROPERTIES,
+                CONFIG_PATH_PREFIX + App.CONSUMER_PROPERTIES), handlerMock, Duration.ofSeconds(5)).consume();
+
+        try (val lines = Files.lines(producerConfigPath)) {
+            verify(handlerMock, times((int) lines.count())).accept(any());
+        }
     }
 
 }
